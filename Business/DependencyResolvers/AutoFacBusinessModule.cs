5c57ed21d88beff2408f85eefe20b182404e5fc2
dbst-6313 <birisierikli@hotmail.com>
2022-09-18 16:09:40 +0300

    Add project files.

---
+++ Business/DependencyResolvers/AutoFacBusinessModule.cs
@@ -0,0 +1,30 @@
+﻿using Autofac;
+using Autofac.Extras.DynamicProxy;
+using Business.Abstract;
+using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
+using Core.Utilities.Security.JWT;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule :Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+      
+
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
+                .EnableInterfaceInterceptors(new ProxyGenerationOptions()
+                {
+                    Selector = new AspectInterceptorSelector()
+                }).SingleInstance();
+
+        }
+    }
+}
---
+++ Core/Utilities/Constants/Messages.cs
@@ -0,0 +1,38 @@
+﻿using Core.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Core.Utilities.Constants
+{
+    public class Messages
+    {
+        //Answer
+        public static string AnswerAdded = "Cevap eklendi";
+        public static string StateAccept = "Başvuru durumu:Kabul edildi";
+        public static string StateDenied = "Başvuru durumu:Reddedildi";
+        public static string AnswerDeleted = "Cevap silindi";
+        public static string AnswerListed = "Cevaplar listelendi";
+        public static string AnswerUpdated = "Cevaplar güncellendi";
+
+        //Auth
+        public static string AuthRegister = "Kullanıcı başarıyla kaydedildi";
+        public static string AuthLogin = "Kullanıcı başarıyla giriş yaptı";
+        public static string PasswordError = "Şifre hatalı";
+        public static string UserNotFound = "Kullanıcı bulunamadı";
+        public static string UserAlreadyExists = "Bu kullanıcı zaten mevcut";
+        public static string AccessTokenCreated = "Access token başarıyla oluşturuldu";
+
+        //Question
+        public static string QuestionAdded = "Soru eklendi";
+        public static string QuestionDeleted = "Soru silindi";
+        public static string QuestionListed = "Soruları listelendi";
+        public static string QuestionUpdate = "Soru güncellendi";
+
+        //User
+        public static string UserAdded = "Kullanıcı eklendi";
+        public static string UserDeleted = "Kullanıcı silindi";
+
+    }
+}
