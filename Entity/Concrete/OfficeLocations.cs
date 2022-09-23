9797beab7402f607821cc245e264434abb59b833
dbst-6313 <birisierikli@hotmail.com>
2022-09-23 15:37:13 +0300

    Entity Layer Done 15.37 23.09

---
+++ Entity/Concrete/OfficeLocations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Concrete
+{
+    public class OfficeLocations
+    {
+        public int Id { get; set; }
+        public string? City { get; set; }
+        public string? District { get; set; }
+        public string? Quarter { get; set; }
+        public string? GoogleMapsLink { get; set; }
+    }
+}
---
+++ Entity/DTOs/AnnouncementDto.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.DTOs
+{
+    public class AnnouncementDto:IDto
+    {
+        public int Id { get; set; }
+        public string? AttorneyName { get; set; }
+        public string? AttorneyLastName { get; set; }
+        public string? AttorneyPhone { get; set; }
+        public string? AttorneyMail { get; set; }
+        public string? AttorneyTitle { get; set; }
+        public string? AnnouncementHeader { get; set; }
+        public string? AnnouncementTitle { get; set; }
+    }
+}
